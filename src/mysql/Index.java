b00eeb7492ba6dfe51780550cbacfcfb3170a45c
Matias-Bernal <payomaty666@gmail.com>
2015-10-31 22:09:32 -0300

    Actualizacion Parcial

--- src/mysql/Index.java
+++ src/mysql/Index.java
@@ -1,5 +1,35 @@
 package src.mysql;
 
+import java.util.LinkedList;
+
 public class Index {
+	private String name;
+	private LinkedList<Column> colums;
+
+	public Index() {
+		super();
+	}
+
+	public Index(String name, LinkedList<Column> colums) {
+		super();
+		this.name = name;
+		this.colums = colums;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LinkedList<Column> getColums() {
+		return colums;
+	}
+
+	public void setColums(LinkedList<Column> colums) {
+		this.colums = colums;
+	}
 
 }
--- src/mysql/PrimaryKey.java
+++ src/mysql/PrimaryKey.java
@@ -1,5 +1,36 @@
 package src.mysql;
 
+import java.util.LinkedList;
+
 public class PrimaryKey {
+
+	private String name;
+	private LinkedList<Column> colums;
+
+	public PrimaryKey() {
+		super();
+	}
+
+	public PrimaryKey(String name, LinkedList<Column> colums) {
+		super();
+		this.name = name;
+		this.colums = colums;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LinkedList<Column> getColums() {
+		return colums;
+	}
+
+	public void setColums(LinkedList<Column> colums) {
+		this.colums = colums;
+	}
 
 }
