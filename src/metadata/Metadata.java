e77dcb8299200eafe7e8dc310d00bdea90bc7cb3
Felipe <astorgadm@gmail.com>
2015-11-04 22:05:49 -0300

    Add Metadata.java class

---
+++ src/metadata/Metadata.java
@@ -0,0 +1,18 @@
+package src.metadata;
+
+import com.mysql.jdbc.DatabaseMetaData;
+
+public class Metadata {
+
+	private DatabaseMetaData matadata;
+	private String[] types = {"TABLE"};
+	
+	public Metadata() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+
+
+}
