5812ac5be6e40a902f8da082e45892228b15c9da
Matias-Bernal <payomaty666@gmail.com>
2015-11-11 18:59:11 -0300

    Agregado el tipo Type.java ademas se agrego el metodo getcolums en MEtadata.java que completa todas las columnas de una tabla.

--- src/metadata/Metadata.java
+++ src/metadata/Metadata.java
@@ -2,18 +2,18 @@ package src.metadata;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.LinkedList;
 
-import com.mysql.jdbc.DatabaseMetaData;
-
 import src.DatabaseConnection;
-import src.mysql.Schema;
+import src.mysql.Column;
 import src.mysql.Table;
+import src.mysql.Type;
+import src.mysql.SQLType;
 
 public class Metadata {
 
-	private  String schema;
-	private String[] types = {"TABLE"};
+	private String schema;
 	private DatabaseConnection databaseconnection;
 
 	public Metadata() {
@@ -44,17 +44,42 @@ public class Metadata {
 
 	public LinkedList<Table> getTables() {
 		LinkedList<Table> tables = new LinkedList<Table>();
+		String[] types = {TableType.TABLE.toString()};
 		try {
-			ResultSet rs = databaseconnection.newMataData().getTables(null, schema,"%", types);
-			while (rs.next()) {
+			ResultSet rst = databaseconnection.newMataData().getTables(null, schema,"%", types);
+			while (rst.next()) {
 				Table tabla = new Table();
-				tabla.setName(rs.getString(3));
+				tabla.setName(rst.getString(3)); //Nombre
+				tabla.setColums(getColums(tabla.getName())); //Columnas
+//				tabla.setPrimarykey(getStprimaryKey(tabla.getName())); //Primary Key
+//				tabla.setIndexs(getIndex(tabla.getName())); //Indexs
+				
 				tables.add(tabla);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return tables;
+	}
+	
+	public LinkedList<Column> getColums(String table_name) {
+		LinkedList<Column> columns = new LinkedList<Column>();
+		try {
+			ResultSet rsc = databaseconnection.newMataData().getColumns(null, schema, table_name, "%");
+			while(rsc.next()){
+ 				Column columna = new Column();
+				columna.setName(rsc.getString("COLUMN_NAME"));
+				Type type = new Type(SQLType.getType(rsc.getInt("DATA_TYPE")), Integer.valueOf(rsc.getInt("COLUMN_SIZE")));	
+				columna.setType(type);
+				columna.setDefaultvalue(rsc.getString("COLUMN_DEF"));
+				if(rsc.getInt("NULLABLE")==1) //0 = NOT NULL 1= NULLEABLE
+					columna.setNullable(true);
+				columns.add(columna);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return columns;
 	}
 
 }
